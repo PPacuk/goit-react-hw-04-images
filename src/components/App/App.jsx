7022007e95ff93935638cce383c46f93a47c1675
Piotr Pacuk <105542652+PPacuk@users.noreply.github.com>
2023-06-18 20:17:32 +0200

    Update App.jsx
    Added api key and url

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,6 +7,12 @@ import Button from 'components/Button/Button';
 
 export default class App extends Component {
   render() {
+    const API_KEY = '35040895-5a8e4f49ce4c30427977eed8e';
+    const API_URL = `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+
+console.log(API_URL);
+
+    
     return (
       <div className={css.mainContainer}>
         <Searchbar />
