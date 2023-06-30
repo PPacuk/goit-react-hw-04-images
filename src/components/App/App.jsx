860df6b7f1e2019954803dfc5913ff79b65e554d
Piotr Pacuk <piotrek.pacuk@gmail.com>
2023-06-30 18:01:47 +0200

    little clean up

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { PropTypes } from 'prop-types';
-import css from './App.module.css';
 import {Searchbar} from 'components/Searchbar/Searchbar';
 import {ImageGallery} from 'components/ImageGallery/ImageGallery';
 import {Button} from 'components/Button/Button';
 import {Loader} from 'components/Loader/Loader';
-import fetchGalleryByQuery from 'services/api';
+import {fetchGalleryByQuery} from 'services/api';
 import {Modal} from 'components/Modal/Modal';
+import css from './App.module.css';
 
 export const App = () => {
   const [gallery, setGallery] = useState([]);
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-const fetchGalleryByQuery = async (searchQuery, page) => {
+export const fetchGalleryByQuery = async (searchQuery, page) => {
   const API_KEY = '35040895-5a8e4f49ce4c30427977eed8e';
   const response = await axios.get(
     `?q=${searchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
@@ -10,4 +10,4 @@ const fetchGalleryByQuery = async (searchQuery, page) => {
   return response.data.hits;
 };
 
-export default fetchGalleryByQuery;
+
